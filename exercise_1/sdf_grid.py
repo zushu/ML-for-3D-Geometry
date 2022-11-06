0d3fed9b7da0b0289509514bcc541dff5b2bbfdf
zushu <zumrudshukurlu@gmail.com>
2022-11-06 23:12:48 +0100

    part 1

---
+++ exercise_1/sdf_grid.py
@@ -0,0 +1,25 @@
+"""Creating an SDF grid"""
+import numpy as np
+
+
+def sdf_grid(sdf_function, resolution):
+    """
+    Create an occupancy grid at the specified resolution given the implicit representation.
+    :param sdf_function: A function that takes in a point (x, y, z) and returns the sdf at the given point.
+    Points may be provides as vectors, i.e. x, y, z can be scalars or 1D numpy arrays, such that (x[0], y[0], z[0])
+    is the first point, (x[1], y[1], z[1]) is the second point, and so on
+    :param resolution: Resolution of the occupancy grid
+    :return: An SDF grid of specified resolution (i.e. an array of dim (resolution, resolution, resolution) with positive values outside the shape and negative values inside.
+    """
+
+    # ###############
+    # TODO: Implement
+#    raise NotImplementedError
+    voxel_coords = np.linspace(-0.5, 0.5, num=resolution)
+    xx, yy, zz = np.meshgrid(voxel_coords, voxel_coords, voxel_coords, indexing='ij')
+    
+    grid = sdf_function(xx.flatten(), yy.flatten(), zz.flatten()).reshape(resolution, resolution, resolution)
+             
+    return grid
+        
+    # ###############
